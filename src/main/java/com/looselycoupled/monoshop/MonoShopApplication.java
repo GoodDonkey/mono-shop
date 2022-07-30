78a0a17aac35f0e519b10d7bab64db495649f4f8
GoodDonkey <president20500@gmail.com>
2022-07-30 17:53:12 +0900

    initial commit

---
+++ src/main/java/com/looselycoupled/monoshop/MonoShopApplication.java
@@ -0,0 +1,13 @@
+package com.looselycoupled.monoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoShopApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MonoShopApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/looselycoupled/monoshop/MonoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.looselycoupled.monoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonoShopApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
