601e5cee35e10f09af0753abc238de86e1b09e8b
GoodDonkey <president20500@gmail.com>
2022-08-01 14:55:25 +0900

    fix: jwt login

--- src/main/java/com/looselycoupled/monoshop/application/users/JwtAuthService.java
+++ src/main/java/com/looselycoupled/monoshop/application/users/JwtAuthService.java
@@ -2,5 +2,6 @@ package com.looselycoupled.monoshop.application.users;
 
 public interface JwtAuthService {
     
+    SignupResponse signup(SignupRequest request);
     JwtLoginResponse login(LoginRequest request);
 }
---
+++ src/main/java/com/looselycoupled/monoshop/application/users/SignupResponse.java
@@ -0,0 +1,10 @@
+package com.looselycoupled.monoshop.application.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SignupResponse {
+    private String username;
+}
